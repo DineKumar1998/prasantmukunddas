6ff76dd1cc13b0b779fcc977f8a721c02ae08b4f
DineKumar1998 <dineshkumar.webhopers@gmail.com>
2022-10-11 13:23:03 +0530

    Some others changes

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000/api' : 'https://web-rho-tawny.vercel.app/api';
+export const server = dev ? 'http://localhost:3000/api' : 'https://prasantmukunddas.vercel.app/api';
