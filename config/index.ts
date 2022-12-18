262b060f17c0f93e47583c70bfa0300296d8f453
DineKumar1998 <dineshkumar.webhopers@gmail.com>
2022-12-18 14:50:00 +0530

    Changes

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000/api' : 'https://prasantmukunddas.vercel.app/api';
+export const server = dev ? 'http://localhost:3000/api' : 'https://prasantmukunddas-94g5sc8ff-dinekumar1998.vercel.app/api';
